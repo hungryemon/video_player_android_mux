2913ba3daa040d507bb7c845a299566c91304c39
Md Shahed Uddin Emon <emon.ostad@gmail.com>
2023-05-28 15:39:49 +0600

    fix: platform_interface_package_name changed

--- test/android_video_player_test.dart
+++ test/android_video_player_test.dart
@@ -10,7 +10,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:video_player_android/src/messages.g.dart';
 import 'package:video_player_android/video_player_android.dart';
-import 'package:video_player_platform_interface/video_player_platform_interface.dart';
+import 'package:video_player_platform_interface_mux/video_player_platform_interface.dart';
 
 import 'test_api.g.dart';
 
